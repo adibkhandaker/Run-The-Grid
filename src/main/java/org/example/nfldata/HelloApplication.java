c386dd8a41626ecf6666330026ee6a15c23560a6
Adib Khandaker <adibkhandaker06@gmail.com>
2025-07-07 23:38:02 -0400

    Update HelloApplication and MainMenu UI components

--- src/main/java/org/example/nfldata/HelloApplication.java
+++ src/main/java/org/example/nfldata/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("NFL DATA HUB");
+        stage.setTitle("Run the Grid");
         stage.setScene(scene);
         stage.show();
     }
